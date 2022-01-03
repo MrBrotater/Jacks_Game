290db787fe55b4f8a47558c9b1ca2090bc3d8b1c
MrBrotater <srgerral@gmail.com>
2022-01-02 21:11:37 -0500

    changes, not working atm

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ WHITE = [255, 255, 255]
 MUSIC = True
 
 # USER EVENTS -------------------------------------------------------------------------------------
-PLAYER_SHOT = pygame.USEREVENT + 1
+NEXT_GAME_PHASE = pygame.USEREVENT + 1
+PLAYER_SHOT = pygame.USEREVENT + 2
 
 
 # BACKGROUND IMAGES ------------------------------------------------------------------------------
-LEVEL_1_BG = pygame.transform.scale(
+BG_IMAGES = dict()
+
+BG_IMAGES[0] = pygame.transform.scale(
+    pygame.image.load(os.path.join('Images', 'Title_screen.png')), (WIN_WIDTH, WIN_HEIGHT))
+
+BG_IMAGES[1] = pygame.transform.scale(
     pygame.image.load(os.path.join('Images', 'Space_background.png')), (WIN_WIDTH, WIN_HEIGHT))
 
 
@@ -39,15 +45,17 @@ class PlayerShip:
         self.gun_positions = {
             -1: (self.x + self.width, self.y + self.height * 0.33),
             1: (self.x + self.width, self.y + self.height * 0.66)}
-        self.shots_on_screen = []
         self.max_shots = 6
 
-    def shoot(self):
-        self.gun_positions = {
-            -1: (self.x + self.width, self.y + self.height * 0.33),
-            1: (self.x + self.width, self.y + self.height * 0.66)}
-        self.shots_on_screen.append(PlayerShot(self.gun_positions[self.shot_toggle]))
-        self.shot_toggle = self.shot_toggle * -1
+    def shoot(self, shots_on_screen):
+        if len(shots_on_screen) < self.max_shots:
+            self.gun_positions = {
+                -1: (self.x + self.width, self.y + self.height * 0.33),
+                1: (self.x + self.width, self.y + self.height * 0.66)}
+            shot = Shot('player', self.gun_positions[self.shot_toggle])
+            self.shot_toggle = self.shot_toggle * -1
+            return shot
+        return
 
     def update(self, keys_pressed):
         if keys_pressed[pygame.K_LEFT] and self.x - self.speed > 0:  # LEFT
@@ -59,25 +67,40 @@ class PlayerShip:
         if keys_pressed[pygame.K_DOWN] and self.y + self.speed + self.height < WIN_HEIGHT:  # DOWN
             self.y += self.speed
 
-        for shot in self.shots_on_screen:
-            shot.update()
-            shot.draw()
-            if shot.x > WIN_WIDTH - shot.width:
-                self.shots_on_screen.remove(shot)
+        # for shot in self.shots_on_screen:
+        #     shot.update()
+        #     shot.draw()
+        #     if shot.x > WIN_WIDTH - shot.width:
+        #         self.shots_on_screen.remove(shot)
 
     def draw(self):
         WIN.blit(self.image, (self.x, self.y))
 
 
-class PlayerShot:
-    def __init__(self, gun_position):
-        self.height = 10
-        self.width = 50
-        self.x, self.y = gun_position
-        self.speed = 10
+class Shot:
+    def __init__(self, shot_type, starting_coords):
+        self.shot_type = shot_type
+        self.shot_dict = {
+            'player': {
+                'height': 10,
+                'width': 50,
+                'image': 'Green_laser_200x38.png',
+                'speed': 10},
+            'enemy': {
+                'height': 10,
+                'width': 50,
+                'image': 'Red_laser_200x37.png',
+                'speed': -10
+            }
+        }
+        self.height = self.shot_dict[self.shot_type]['height']
+        self.width = self.shot_dict[self.shot_type]['width']
+        self.x, self.y = starting_coords
+        self.speed = self.shot_dict[self.shot_type]['speed']
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.image = pygame.transform.scale(
-            pygame.image.load(os.path.join('Images', 'Green_laser_200x38.png')), (self.width, self.height))
+            pygame.image.load(os.path.join(
+                'Images', self.shot_dict[self.shot_type]['image'])), (self.width, self.height))
 
     def update(self):
         self.x += self.speed
@@ -85,41 +108,84 @@ class PlayerShot:
     def draw(self):
         WIN.blit(self.image, (self.x, self.y))
 
-# DRAWING -------------------------------------------------------------------------------------------
-def draw(objects_on_screen, keys_pressed):
-    WIN.blit(LEVEL_1_BG, (0, 0))
-    for obj in objects_on_screen:
-        obj.update(keys_pressed)
-        obj.draw()
-    pygame.display.update()
-    return
 
+class MainApp:
+    def __init__(self, window):
+        self.clock = pygame.time.Clock()
+        self.win = window
+        self.run = True
+        self.level = 0
+        self.player = PlayerShip()
+        self.ships_on_screen = []
+        self.player_shots_on_screen = []
+        self.enemy_shots_on_screen = []
+        self.BG_IMAGES = {
+            0: pygame.transform.scale(
+                pygame.image.load(os.path.join('Images', 'Title_screen.png')), (WIN_WIDTH, WIN_HEIGHT)),
+            1: pygame.transform.scale(
+                pygame.image.load(os.path.join('Images', 'Space_background.png')), (WIN_WIDTH, WIN_HEIGHT))}
 
-# MAIN LOOP -------------------------------------------------------------------------------------------
-def main():
-    clock = pygame.time.Clock()
-    run = True
-    player = PlayerShip()
-    objects_on_screen = [player]
-
-    while run:
-        clock.tick(FPS)
+    def check_events(self):
+        event_dict = {
+            pygame.QUIT: self.event_quit,
+            NEXT_GAME_PHASE: self.event_next_phase,
+        }
 
         for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
+            if event.type in event_dict:
+                event_dict[event.type]()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and len(player.shots_on_screen) < player.max_shots:
-                    player.shoot()
+                if event.key == pygame.K_SPACE:
+                    shot = self.player.shoot(self.player_shots_on_screen)
+                    self.player_shots_on_screen.append(shot)
+        return
 
-        keys_pressed = pygame.key.get_pressed()
-        draw(objects_on_screen, keys_pressed)
+    def event_quit(self):
+        self.run = False
+        pygame.quit()
+        return
 
-    return
+    def event_next_phase(self):
+        self.level += 1
+        return
 
+    def update_shots(self):
+        for shot in self.player_shots_on_screen:
+            shot.update()
+            if shot.x > WIN_WIDTH - shot.width:
+                self.player_shots_on_screen.remove(shot)
+        for shot in self.enemy_shots_on_screen:
+            shot.update()
+            if shot.x < 0:
+                self.enemy_shots_on_screen.remove(shot)
+        return
+
+    def draw(self, keys_pressed):
+        self.win.blit(self.BG_IMAGES[self.level])
+        if self.level > 0:
+            self.player.update(keys_pressed)
+            self.player.draw()
+            self.update_shots()
+            for ship in self.ships_on_screen:
+                ship.draw()
+            for shot in self.player_shots_on_screen:
+                shot.draw()
+            for shot in self.enemy_shots_on_screen:
+                shot.draw()
+        pygame.display.update()
+        return
+
+    def main(self):
+        while self.run:
+            self.clock.tick(FPS)
+            self.check_events()
+            keys_pressed = pygame.key.get_pressed()
+            self.draw(keys_pressed)
+
+        return
+
+
+game = MainApp(WIN)
 
 if __name__ == '__main__':
-    main()
+    game.main()
